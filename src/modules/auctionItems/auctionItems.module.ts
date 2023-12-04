6a5953727317cdf6d21d2190bed67ad9612c67c3
Alja Čekada <alja.cekada@gmail.com>
2023-12-04 15:12:59 -0800

    add notofocations repository to auctionItems module

--- src/modules/auctionItems/auctionItems.module.ts
+++ src/modules/auctionItems/auctionItems.module.ts
@@ -4,11 +4,13 @@ import { AuctionItemsController } from './auctionItems.controller';
 import { AuctionItem } from 'src/entities/auction_item.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
+import { Notification } from 'src/entities/notification.entity';
+import { NotificationsService } from '../notifications/notifications.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AuctionItem, User])],
+  imports: [TypeOrmModule.forFeature([AuctionItem, User, Notification])],
   controllers: [AuctionItemsController],
-  providers: [AuctionItemsService],
+  providers: [AuctionItemsService, NotificationsService],
   exports: [AuctionItemsService],
 })
 export class AuctionItemsModule {}
